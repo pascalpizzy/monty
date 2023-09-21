e6e9ab4d8225ad52aa1ceba8cbc31b2a91a9f60f
pascalpizzy <pascalikechukwu@gmail.com>
2023-09-21 14:39:53 -0700

    monty_pall.c

---
+++ monty_pall.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * m_pall - for printing the stack
+ * @head: head of stack
+ * @counter: no used
+ * Return: no return
+*/
+void m_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
